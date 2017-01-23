a53b0d7d2cef23aea1863a686f3d6a58e6a40e3b
xiplus <huangxuanyuxiplus@gmail.com>
2017-01-23 15:36:10 +0800

    showthis show service id

--- showthis.sh
+++ showthis.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 ip=$(ifconfig | grep -Po "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" | head -1)
-curl "http://dev.tfcis.org/cms/cmsconf.php?showthis&ip="$ip
+echo "This machine ip is "$ip
+
+for service in LogService ResourceService ScoringService Checker EvaluationService Worker ContestWebServer AdminWebServer ProxyService PrintingService TestFileCacher
+do
+	start=$(curl -s "http://dev.tfcis.org/cms/cmsconf.php?getstart&ip="$ip"&start="$service)
+	count=$(curl -s "http://dev.tfcis.org/cms/cmsconf.php?getcount&ip="$ip"&count="$service)
+	if [ "${start}" != "-1" ]; then
+		for (( i=$start; i<$start+$count; i++ ))
+		do
+			echo $service" "$i
+		done
+	fi
+done
 echo ""
