328e2fc1201e86ba1cfc542d1336adefbec6702a
xiplus <huangxuanyuxiplus@gmail.com>
2017-01-23 15:35:21 +0800

    install in one command

--- install.sh
+++ install.sh
@@ -2,38 +2,8 @@
 
 cd ~
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-sudo apt-get update
-sudo apt-get -y install fpc
-sudo apt-get -y install fpc-2.4.4
-sudo apt-get -y install build-essential
-sudo apt-get -y install fpc
-sudo apt-get -y install postgresql
-sudo apt-get -y install postgresql-client
-sudo apt-get -y install gettext
-sudo apt-get -y install python2.7
-sudo apt-get -y install python-setuptools
-sudo apt-get -y install python-tornado
-sudo apt-get -y install python-psycopg2
-sudo apt-get -y install python-sqlalchemy
-sudo apt-get -y install python-psutil
-sudo apt-get -y install python-netifaces
-sudo apt-get -y install python-crypto
-sudo apt-get -y install python-tz
-sudo apt-get -y install python-six
-sudo apt-get -y install iso-codes
-sudo apt-get -y install shared-mime-info
-sudo apt-get -y install stl-manual
-sudo apt-get -y install python-beautifulsoup
-sudo apt-get -y install python-mechanize
-sudo apt-get -y install python-coverage
-sudo apt-get -y install python-mock
-sudo apt-get -y install cgroup-lite
-sudo apt-get -y install python-requests
-sudo apt-get -y install python-werkzeug
-sudo apt-get -y install python-gevent patool
-sudo apt-get -y install tmux
-sudo apt-get -y install vim
-sudo apt-get -y install htop
+sudo apt-get update 
+sudo apt-get -y install fpc build-essential fpc postgresql postgresql-client gettext python2.7 python-setuptools python-tornado python-psycopg2 python-sqlalchemy python-psutil python-netifaces python-crypto python-tz python-six iso-codes shared-mime-info stl-manual python-beautifulsoup python-mechanize python-coverage python-mock cgroup-lite python-requests python-werkzeug python-gevent patool tmux vim htop
 
 wget https://github.com/cms-dev/cms/releases/download/v1.2.0/v1.2.0.tar.gz
 tar -zxvf ./v1.2.0.tar.gz
